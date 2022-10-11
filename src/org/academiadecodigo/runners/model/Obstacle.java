25088000f336e5876b3049f15fac99e698355125
pedroascenso <pedroascensosilva@gmail.com>
2022-10-11 21:26:35 +0100

    collision fixed

--- src/org/academiadecodigo/runners/model/Obstacle.java
+++ src/org/academiadecodigo/runners/model/Obstacle.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.runners.model;
 
 import org.academiadecodigo.runners.grid.position.GridPosition;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Obstacle {
@@ -24,21 +25,30 @@ public class Obstacle {
         this.pic = new Picture(col, row, path.getPath());
         this.pic.draw();
 
+
+
         int i = 0;
 
         while(i <= limit) {
             try {
-                Thread.sleep(400);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            this.pic.translate(0, 100);
+            this.pic.translate(0, 50);
 
+            //System.out.println(pic.getX() + " this is object");
+            //System.out.println(pic.getY() + " this is object");
+
+            //System.out.println(player.getPic().getX());
+            //System.out.println(player.getPic().getY());
             if (checkCollision()) {
                 if(path.getType().equals("GOOD")) {
+                    this.pic.delete();
                     player.setScore(player.getScore() + 1);
                     System.out.println("score " + player.getScore());
                 } else {
+                    this.pic.delete();
                     player.setLives(player.getLives() - 1);
                     System.out.println("lives " + player.getLives());
                     if(player.getLives() < 1) {
@@ -52,10 +62,13 @@ public class Obstacle {
         return true;
         }
 
-
     public boolean checkCollision() {
-        return pic.getX() == player.getPic().getX() && pic.getY() >= player.getPic().getY();
-        //return ((pic.getHeight())/2) == ((player.getPic().getX())*2);
+        if ((pic.getY() == player.getPic().getY()) && (pic.getX() == player.getPic().getX())){
+            return true;
+        }
+        return false;
+
+
     }
 
 
